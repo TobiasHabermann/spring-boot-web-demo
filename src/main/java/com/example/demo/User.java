2d8e6522a4e6adbe05edd6c229f41ebb5fc35125
TobiasHabermann <49537177+TobiasHabermann@users.noreply.github.com>
2025-11-29 12:30:30 +0100

    Create User.java

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String vorname;
+    private Integer alter;
+
+    public User() {}
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getVorname() {
+        return vorname;
+    }
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+    public Integer getAlter() {
+        return alter;
+    }
+    public void setAlter(Integer alter) {
+        this.alter = alter;
+    }
+}
